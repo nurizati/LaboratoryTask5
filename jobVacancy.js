9b7462f8c65e0776f842adf0958099c062f668e1
nurizati <91452441+nurizati@users.noreply.github.com>
2022-01-04 09:17:28 +0800

    TEST

--- jobVacancy.js
+++ jobVacancy.js
@@ -1,4 +1,4 @@
-function onsubmitform() {
+function onsubmitform(){
     var name = document.getElementById("text-basic").value;
     var gender1 = document.getElementById("radio-choice-c").value;
     var gender2 = document.getElementById("radio-choice-d").value;
@@ -9,15 +9,19 @@ function onsubmitform() {
     var language2 = document.getElementById("checkbox-2a").value;
     var language3 = document.getElementById("checkbox-3a").value;
     var language4 = document.getElementById("checkbox-4a").value;
-    if (name && (gender1 || gender2) && exp && (yes || no) && (language1 || language2 || language3 || language4)) {
-        if (confirm("Are you sure to Send?")) {
-            alert("Thank you..");
-        }
-        else {
-            alert("Make sure all information is correct..");
-        }
+    if(name&&(gender1||gender2)&&exp&&(yes||no)&&(language1||language2||language3||language4))
+    {
+    if(confirm("Are you sure to Send?"))
+    {
+    alert("Thank you.");
     }
-    else {
-        alert("Incomplete Form!!");
+    else
+    {
+    alert("Make sure all information is correct.");
     }
-}
+    }
+    else
+    {
+    alert("Incomplete Form!");
+    }
+    }
